6c63a4511243e1852b2e80dc4b773a5cf82f361a
Prarthana Agrawal <agrawalprayer@gmail.com>
2023-09-03 16:22:09 +0530

    63 entries

--- csv_to_html.py
+++ csv_to_html.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-data = pd.read_csv("C:/Users/91940/coursera-html/site/internship-database.csv") #Add the location of your csv file here
+data = pd.read_csv("C:/Users/91940/rekindled/internship-database.csv") #Add the location of your csv file here
 data.fillna("-", inplace=True)
 n = len(data) #number of rows of data to consider
 print(n)
@@ -20,7 +20,7 @@ duration = data['Duration'][:n]
 deadline = data['Deadline'][:n]
 paid = data['Paid'][:n]
 
-html_text_file = open("C:/Users/91940/coursera-html/site/csv_to_html.txt","w")
+html_text_file = open("C:/Users/91940/rekindled/csv_to_html.txt","w")
 
 for i in range(n):
     line1 = "<td> <a href="+str(link[i])+">" +\
@@ -36,6 +36,6 @@ for i in range(n):
     html_text_file.write(each_entry)
 
 html_text_file.close()
-file = open("C:/Users/91940/coursera-html/site/csv_to_html.txt","r")
+file = open("C:/Users/91940/rekindled/csv_to_html.txt","r")
 print(file.readlines())
 file.close()
